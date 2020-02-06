a327e883543f343905b4d6c40dfe9f8bd894e663
Felipe Lima <felipenplima@gmail.com>
2020-02-06 16:42:01 +0000

    presentation version

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,26 +1,28 @@
 class Menu {
   constructor(game) {
     this.game = game;
-    this.screen = 'menu';
+    this.screen = "menu";
     this.image = new Image();
-    this.image.src = 'images/menu.png';
+    this.image.src = "images/menu1.png";
     this.setKeyboardEventListeners();
   }
 
   draw() {
-    window.addEventListener('load', event => {
+    window.addEventListener("load", event => {
+      this.game.context.fillStyle = "red";
+      this.game.context.fillRect(0, 0, 620, 480);
       this.game.context.drawImage(this.image, 0, 0, 620, 480);
     });
-    console.log('menu drawing');
+    console.log("menu drawing");
   }
 
   setKeyboardEventListeners() {
-    window.addEventListener('keydown', event => {
+    window.addEventListener("keydown", event => {
       switch (event.keyCode) {
         case 13:
-          if (this.screen === 'menu') {
+          if (this.screen === "menu") {
             this.game.gameIsRunning = true;
-            this.draw();
+            this.game.reset();
           }
           break;
       }
