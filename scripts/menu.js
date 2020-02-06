c67bbbd8fe44e66b3e26fb4c96ae3d323561ae37
Felipe Lima <felipenplima@gmail.com>
2020-02-06 17:19:33 +0000

    Last try with this commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,21 +3,12 @@ class Menu {
     this.game = game;
     this.screen = "menu";
     this.image = new Image();
-<<<<<<< HEAD
     this.image.src = "images/menu.png";
-=======
-    this.image.src = "images/menu1.png";
->>>>>>> a327e883543f343905b4d6c40dfe9f8bd894e663
     this.setKeyboardEventListeners();
   }
 
   draw() {
     window.addEventListener("load", event => {
-<<<<<<< HEAD
-=======
-      this.game.context.fillStyle = "red";
-      this.game.context.fillRect(0, 0, 620, 480);
->>>>>>> a327e883543f343905b4d6c40dfe9f8bd894e663
       this.game.context.drawImage(this.image, 0, 0, 620, 480);
     });
     console.log("menu drawing");
